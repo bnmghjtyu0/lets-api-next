1a4eee9146c7de869c7a9d60a1d997bb8fc7dfd0
RichardLiao <richard.liao@program.com.tw>
2021-03-03 14:51:41 +0800

    首頁串接 api

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,36 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  const router = useRouter();
-
+export default function Home({ apiHome }) {
+  if (apiHome.retCode === 0) return <div />;
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {apiHome.retVal.title}
     </div>
   );
 }
 
+export async function getStaticProps(ctx) {
+  try {
+    //串接 api
+    const result = await fetch("http://localhost:3001/api").then((res) =>
+      res.json()
+    );
+    // api 回傳成功
+    return {
+      props: {
+        apiHome: result,
+      },
+    };
+  } catch (error) {
+    // 伺服器有問題
+    error.ctx = ctx;
+    // logErrorToExternalLoggingService(error, "Fetching static props failed");
+    throw error;
+  }
+}
