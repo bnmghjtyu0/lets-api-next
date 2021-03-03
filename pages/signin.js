3bc2d9d5c3efe09ca7b2652041c8bb746544d583
RichardLiao <richard.liao@program.com.tw>
2021-03-03 15:36:41 +0800

    call api /api/login

--- pages/signin.js
+++ pages/signin.js
@@ -5,19 +5,22 @@ const LoginPage = () => {
   let formRef = React.useRef();
   async function handleSubmit(e) {
     e.preventDefault();
-    const { email, password } = formRef.current.elements;
-    const res = await fetch("/api/signin", {
+    const { username, password } = formRef.current.elements;
+    const res = await fetch("/api/login", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: email.value, password: password.value }),
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
     })
       .then((res) => res.json())
       .then((json) => json);
     if (res.retCode === 1) {
-      console.log(res.retVal.token);
+      console.log("登入成功");
       router.replace("/");
     }
   }
@@ -26,8 +29,8 @@ const LoginPage = () => {
       <h4>登入</h4>
       <form onSubmit={handleSubmit} ref={formRef}>
         <div className="flex">
-          <label>email</label>
-          <input name="email" />
+          <label>username</label>
+          <input name="username" />
         </div>
         <div>
           <label>password</label>
