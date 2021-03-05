403d876c2ec4a7c8b206bf89e6513ca13b0f3645
RichardLiao <richard.liao@program.com.tw>
2021-03-05 11:04:26 +0800

    create UserContext

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,8 @@ import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
+
+    //載入 styled-component ssr
     const sheet = new ServerStyleSheet();
     const page = renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />)
